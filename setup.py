9f8dfe2dfb9e46ba731b54b8604cfa65c7f28dd7
Amir Hossein Kargaran <kargaranamir@gmail.com>
2021-08-02 18:26:42 +0300

    create skeleton of pip package

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="visual-clutter",
+    version="0.0.1",
+    author="Amir Hossein Kargaran",
+    author_email="kargaranamir@gmail.com",
+    description="Python Implementation of two measures of visual clutter (Feature Congestion and Subband Entropy)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kargaranamir/visual-clutter",
+    project_urls={
+        "Bug Tracker": "https://github.com/kargaranamir/visual-clutter/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Linux/Mac Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
