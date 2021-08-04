8f4092eaaeb3984113b0b3416ef0e016c75a0fce
Amir Hossein Kargaran <kargaranamir@gmail.com>
2021-08-04 11:25:56 +0300

    update dependencies

--- setup.py
+++ setup.py
@@ -18,14 +18,14 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: Linux or on OSX",
+        "Operating System :: Linux or OSX",
     ],
     install_requires=[
-        'numpy',
-        'scipy',
-        'opencv-python',
-        'Pillow',
-        'pyrtools'
+        'numpy' >= 1.20.2,
+        'opencv-python' >= 4.5.3,
+	'scipy' == 1.7.0,
+	'Pillow' >= 8.3.1,
+	'pyrtools' == 1.0.0
     ],
     packages=setuptools.find_packages(),
     keywords = ['visual clutter', 'feature congestion', 'subband entropy'],
